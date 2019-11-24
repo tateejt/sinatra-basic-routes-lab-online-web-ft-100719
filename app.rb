c656a7929cfef54244351344c0406d4f67bf006a
Emery Tate <tateejt@gmail.com>
2019-11-24 19:54:46 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,14 +2,14 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   get '/name' do 
-    
+    "My name is Emery"
   end
   
   get '/hometown' do 
-    
+    "My hometown is Winston-Salem"
   end
   
   get '/favorite-song' do 
-    
+    "My favorite sonf is Bop"
   end
 end
