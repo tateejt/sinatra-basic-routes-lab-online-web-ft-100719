06e5d112956675e62db94f1e4459a5fac264dfc9
Emery Tate <tateejt@gmail.com>
2019-11-24 19:55:09 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do 
+    "My name is Emery"
+  end
+  
+  get '/hometown' do 
+    "My hometown is Winston-Salem"
+  end
+  
+  get '/favorite-song' do 
+    "My favorite song is Bop"
+  end
 end
