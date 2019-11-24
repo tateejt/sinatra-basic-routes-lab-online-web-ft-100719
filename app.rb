a795d68eaffb87a0f18a2c3bb5a2409140f535e0
Emery Tate <tateejt@gmail.com>
2019-11-24 19:53:33 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do 
+    
+  end
+  
+  get '/hometown' do 
+    
+  end
+  
+  get '/favorite-song' do 
+    
+  end
 end
